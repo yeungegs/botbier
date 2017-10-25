9993613ed6d68790e7f11cf358f37486e0073685
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-10-24 18:02:59 -0700

    Update install script - add destination directory and print messages to STDOUT

--- installblz.sh
+++ installblz.sh
@@ -1,22 +1,44 @@
 #!/usr/bin/env bash
 cd ~/
+
 # Download StarCraftII for Linux
-# See https://github.com/Blizzard/s2client-proto for terms and password
 wget "http://blzdistsc2-a.akamaihd.net/Linux/SC2.3.16.1.zip"
-unzip SC2.3.16.1.zip
 
-# Change working directory to "Maps" folder
-cd ~/StarCraftII/Maps/
-
-# Download map packs and extract
+# Download map packs
 wget "http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season1.zip"
 wget "http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season2.zip"
 wget "http://blzdistsc2-a.akamaihd.net/MapPacks/Ladder2017Season3.zip"
 wget "http://blzdistsc2-a.akamaihd.net/MapPacks/Melee.zip"
-unzip Ladder2017Season1.zip
-unzip Ladder2017Season2.zip
-unzip Ladder2017Season3.zip
-unzip Melee.zip
+
+# Unzip archives - see https://github.com/Blizzard/s2client-proto for terms and password
+unzip SC2.3.16.1.zip -d ~
+echo -e "##############################
+\n
+\n
+SUCCESS: SC2 extracted
+\n
+\n
+##############################"
+
+unzip Ladder2017Season1.zip -d ~/StarCraftII/Maps/
+unzip Ladder2017Season2.zip -d ~/StarCraftII/Maps/
+unzip Ladder2017Season3.zip -d ~/StarCraftII/Maps/
+echo -e "##############################
+\n
+\n
+SUCCESS: Ladder Maps extracted
+\n
+\n
+##############################"
+
+unzip Melee.zip -d ~/StarCraftII/Maps/
+echo -e "##############################
+\n
+\n
+SUCCESS: Melee Maps extracted
+\n
+\n
+##############################"
 
 # Remove zip files
 rm *.zip
